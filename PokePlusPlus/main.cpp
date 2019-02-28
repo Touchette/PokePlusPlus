bfea40b26129b8f547d1af163301118e6d12c964
Nat <marionettis2@gmail.com>
2019-02-28 02:10:41 -0800

    fixed project files
    visual studio is pretty much a cancer to work with but I think it's actually working  now

--- PokePlusPlus/main.cpp
+++ PokePlusPlus/main.cpp
@@ -1,6 +1,7 @@
-#include <SFML\Graphics.hpp>
+#include <SFML/Graphics.hpp>
 
-int main() {
+int main()
+{
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
