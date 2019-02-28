eba821e4d7cbdefc5484c025522972faf56c3711
Nat <marionettis2@gmail.com>
2019-02-28 03:18:02 -0800

    added may as a player, added movement
    may's idle sprites are in and she is the default character for now, she can also move around and swap between idle animations

--- PokePlusPlus/main.cpp
+++ PokePlusPlus/main.cpp
@@ -2,7 +2,18 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-	sf::RenderWindow window(sf::VideoMode(512, 512), "PokéPlusPlus", sf::Style::Close | sf::Style::Titlebar);
+	sf::RenderWindow window(sf::VideoMode(256, 256), "PokéPlusPlus", sf::Style::Close | sf::Style::Titlebar);
+
+	sf::RectangleShape player(sf::Vector2f(14.0f, 20.0f));
+	sf::Texture MayIdleTexture;
+	sf::Texture MayLeftTexture;
+	sf::Texture MayRightTexture;
+	sf::Texture MayBackTexture;
+	MayIdleTexture.loadFromFile("../Sprites/may-idle-forward.png");
+	MayLeftTexture.loadFromFile("../Sprites/may-idle-left.png");
+	MayRightTexture.loadFromFile("../Sprites/may-idle-right.png");
+	MayBackTexture.loadFromFile("../Sprites/may-idle-back.png");
+	player.setTexture(&MayIdleTexture);
 
 	sf::Image icon;
 	if (!icon.loadFromFile("../Sprites/icon.png")) {
@@ -22,8 +33,34 @@ int main(int argc, char *argv[]) {
 				case sf::Event::Closed: 
 					window.close();
 					break;
+				case sf::Event::TextEntered:
+					if (evnt.text.unicode < 128) {
+						//printf("%c", evnt.text.unicode);
+					}
+					break;
 			}
 		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+			player.setTexture(&MayBackTexture);
+			player.move(sf::Vector2f(0, -0.01f));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+			player.setTexture(&MayLeftTexture);
+			player.move(sf::Vector2f(-0.01f, 0));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+			player.setTexture(&MayIdleTexture);
+			player.move(sf::Vector2f(0, 0.01f));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+			player.setTexture(&MayRightTexture);
+			player.move(sf::Vector2f(0.01f, 0));
+		}
+
+		window.clear();
+		window.draw(player);
+		window.display();
 	}
 
 	return 0;
